52a807155c356c375f31dff4c863ae0bbf96f967
Aran Deltac <bluefeet@gmail.com>
2015-06-24 12:41:17 -0700

    Add author and license bits.

--- lib/Web/Starch/Store/CHI.pm
+++ lib/Web/Starch/Store/CHI.pm
@@ -164,3 +164,21 @@ sub remove {
 }
 
 1;
+__END__
+
+=head1 AUTHOR
+
+Aran Clary Deltac <bluefeetE<64>gmail.com>
+
+=head1 ACKNOWLEDGEMENTS
+
+Thanks to L<ZipRecruiter|https://www.ziprecruiter.com/>
+for encouraging their employees to contribute back to the open
+source ecosystem.  Without their dedication to quality software
+development this distribution would not exist.
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
